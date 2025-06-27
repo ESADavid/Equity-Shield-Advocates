3f1f9d43f9a9483d0431bb0321f0224d1f16aad6
Sean Broome <bsean4890@gmail.com>
2025-06-27 15:29:33 -0400

    UPDATE

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,7 @@ module.exports = defineConfig({
     baseUrl: 'http://localhost:4000',
     specPattern: 'cypress/e2e/**/*.cy.js',
     supportFile: false,
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 20000,
   },
 });
