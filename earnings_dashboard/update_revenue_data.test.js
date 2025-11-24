665c400d7cbc2813640429b610e70c11dc6d2a10
Sean Broome <bsean4890@gmail.com>
2025-11-23 18:45:31 -0800

    UPDATE

--- earnings_dashboard/update_revenue_data.test.js
+++ earnings_dashboard/update_revenue_data.test.js
@@ -1,10 +1,10 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
+const __importDefault = (mod) => {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const promises_1 = __importDefault(require("fs/promises"));
-const path_1 = __importDefault(require("path"));
+const promises_1 = __importDefault(require("node:fs/promises"));
+const path_1 = __importDefault(require("node:path"));
 const update_revenue_data_1 = __importDefault(require("./update_revenue_data"));
 const testDataPath = path_1.default.resolve(__dirname, '../../owlban_repos/test_revenue.json');
 // Mock data for testing
